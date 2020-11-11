e40f4982b94222d873e53e330048388435f5c3fe
dbouris <bourisd2@gmail.com>
2020-11-11 20:59:10 +0200

    CREATING THE FILES

---
+++ calling.java
@@ -0,0 +1,9 @@
+
+public class calling {
+
+public static void main(String[] args {
+	
+    
+}
+}
+
---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables {
+   
+	public string name;	
+	public int age;
+
+	public variables(name,age){
+	this.name = name;
+	this.age = age;
+	
+	}
+
+
+}
