43f83cfc1712e0275c9528ab4a9eb3388ead9dfc
dbouris <bourisd2@gmail.com>
2020-11-11 21:16:44 +0200

    CHANGES TO THE MAIN CLASS

--- calling.java
+++ calling.java
@@ -3,6 +3,11 @@ public class calling {
 
 public static void main(String[] args {
 	
+	variables pers1 = new variables ("DIMITRIS BOURIS",19);
+	variables pers2 = new variables ("GIANNIS PETROPOULOS",20);
+	System.out.println(pers1);
+	system.out.println(pers2);
+
     
 }
 }
