91c4caaaf3ae318c959eeeceb07d010682358f12
dbouris <bourisd2@gmail.com>
2020-11-11 21:55:29 +0200

    FIXING ERRORS

--- calling.java
+++ calling.java
@@ -1,7 +1,7 @@
 
 public class calling {
 
-public static void main(String[] args {
+public static void main(String[] args) {
 	
 	variables pers1 = new variables ("DIMITRIS BOURIS",19);
 	variables pers2 = new variables ("GIANNIS PETROPOULOS",20);
